60a6c2d87f0d8f5d2bd62fe9c605a1b1e4eb8db0
houckontape <david.esteve66@gmail.com>
2019-06-23 00:52:54 +0200

    affichage du mot a devinner ok

--- pendu.js
+++ pendu.js
@@ -3,30 +3,79 @@
 //simplon narbonne 2019 la prairie//
 
 //declaration des variables de jeu
-var wordsToGuess = ['panier','tribut','plumes','pistes','dormir'];// le mot à deviner
-var saissieLetter = "";// la lettre saissie
-var numberGame = " ";//nombre de partie
-//declaration de la variables canvas
-var canvas = "document.getElementById('gamescreen')";
-var ctx = "canvas.getContext('2d')";
-
-//declaration de la fonction main
-//la fonction doit choisir un mot dans la wordsToGuess
+// liste de mot à devinner
+var wordsToGuess = ['panier','tribut','plumes','pistes','dormir'];
+// Le mot a devinner
+var secretWord = " ";
+//lettre entrer par l'utilisateur
+var saissieLetter = "_";
+// nombre de partie
+var gameNumber = 0;
+//liste des lettre essayer
+var letterTry = [];;
+// créer un objet map
+var mot =new Map();
+//boucle pour remplir l'objet d'underscore
+for (var i = 0; i < 6; i++) {
+  mot.set(i,"_");
+}
+console.log(mot);//sert de test
+//fin des variables
+//___________________________________________________________________________
+//fonction permettant de dessiner le mot dans le canvas
+function draw(){
+  var canvas = document.getElementById('gamescreen');
+  var ctx = canvas.getContext('2d');
+  ctx.font = '48px serif';
+  ctx.fillText(mot.get(0), 10, 350);
+  ctx.fillText(mot.get(1),60 , 350);
+  ctx.fillText(mot.get(2),100 ,350);
+  ctx.fillText(mot.get(3),150 ,350);
+  ctx.fillText(mot.get(4),200 ,350);
+  ctx.fillText(mot.get(5),250 ,350);
+}
+//la fonction doit choisir un mot aleatoire dans la wordsToGuess
 function wordselect(){
-  //fonction pour generé un index aleatoire
   function rIndex(max){
     return Math.floor(Math.random()* Math.floor(max));
   }
-  //returne un mot aleatoire en foncion de la longeur de la liste
     return wordsToGuess[rIndex(wordsToGuess.length)];
-  //testé avec console.log()ok
 }
-// analysé si la saissieLetter est dans le mot
+secretWord = wordselect();
+//todo:ecrire la fonction pour dessiner le Pendu
+//__________________________________________________
+// function recupere la saissie clavier et joue
+function saissie(){
+  document.addEventListener("keypress", function (e) {
+    console.log("Vous avez appuyé sur la touche " + String.fromCharCode(e.charCode));
+    saissieLetter = String.fromCharCode(e.charCode);
+    letterTry.push(saissieLetter);//ajoute la lettre saissie dans une liste(type:array)
+    console.log(letterTry);//decommenter pout voir les lettres tester
+     var found = secretWord.search(saissieLetter);//recupere l'indice de la lettre
+     //dans le mot secretWord si la lettre n est pas dans le mot renvoie -1
+     console.log(found);//affiche l'indice de la lettre taper
+     mot.set(found,saissieLetter);//dans le ojet map vide on ajoute la lettre a l endroit voulue
+     if (found == -1){
+       console.log("perdu");
+     }else{
+       console.log ("bien");
+       console.log (mot);
+       draw();
+       }
+     })
+  }
+//____________________________________________________________________________
 
-//si oui returne la lettre
-//si non return une image dans le canvas
+
+
 
 //fonction qui compte les parties si >5 stop
 
 
-//affichage avec canva
+
+
+//TEST:commande pour controler les actions dans la console
+console.log (wordsToGuess);//decommenter pour voir la liste de mots
+console.log ("mon mot secret est " + secretWord);//decommenter pour voir le mot a trouver
+console.log ("lettre saissie "+ saissieLetter);
+saissie();
